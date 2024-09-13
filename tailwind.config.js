d76668ae144fb973e086449e44dbeaf65c97cb98
Bilek-Petr <p.bilek29@gmail.com>
2024-09-13 17:10:58 +0200

    data setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,21 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      container: {
+        padding: "2rem",
+      },
       fontFamily: {
         heading: ["Archivo Narrow", "system-ui", "serif"],
         special: ["Playfair", "system-ui", "serif"],
         text: ["Archivo", "system-ui", "sans-serif"],
       },
       colors: {
-        mainBlack: "#1E1E1E",
+        mainAccent: "#1E1E1E",
+      },
+      fontSize: {
+        nameHeading: "clamp(5rem, 23vw, 16rem)",
+        descriptionText: "clamp(1.5rem, 3vw, 5rem)",
+        navText: "clamp(3.25rem, 15vw, 6rem)",
       },
     },
   },
