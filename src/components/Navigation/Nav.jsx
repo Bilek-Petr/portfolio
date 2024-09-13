0347f1b76894437186baeca730fe5b1df1cd629c
Bilek-Petr <p.bilek29@gmail.com>
2024-09-13 21:19:33 +0200

    closing/opening of nav fixed

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavList from "./NavList";
 
 export default function Nav({ items }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  useEffect(() => {
+    return () => setIsExpanded(false);
+  }, []);
+
   return (
     <nav className="sm:flex sm:justify-between sm:px-4">
-      <NavList items={items} />
+      <NavList
+        items={items}
+        toggleExpanded={toggleExpanded}
+        isExpanded={isExpanded}
+      />
     </nav>
   );
 }
--- src/components/Navigation/NavList.jsx
+++ src/components/Navigation/NavList.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import NavItem from "./NavItem";
 import { IoMenuOutline } from "react-icons/io5";
 
-export default function NavList({ items }) {
+export default function NavList({ items, toggleExpanded, isExpanded }) {
   return (
     <>
       <div className="flex items-center justify-between px-4 py-4 sm:py-3">
         <p className="cursor-pointer text-2xl font-bold sm:text-lg">Logo</p>
-        <button className="sm:hidden" aria-label="Toggle menu">
+        <button
+          className="sm:hidden"
+          aria-label="Toggle menu"
+          onClick={toggleExpanded}
+        >
           <IoMenuOutline className="size-8" />
         </button>
       </div>
 
-      <ul className="sm:flex sm:items-center sm:gap-4">
+      <ul
+        className={`${
+          isExpanded ? "block" : "hidden"
+        } sm:flex sm:items-center sm:gap-4`}
+      >
         {items.map((item) => (
           <NavItem key={item.id} title={item.title} link={item.link} />
         ))}
