15cd8a2bc736a5093c6c7964f900542837eb4bb4
Bilek-Petr <p.bilek29@gmail.com>
2024-09-16 12:41:06 +0200

    fixed text sizing to clamp

--- src/components/Common/SectionBanner.jsx
+++ src/components/Common/SectionBanner.jsx
@@ -14,18 +14,21 @@ export default function SectionBanner({ bannerTitle, children }) {
         className="flex h-[9rem] items-center justify-between border-t-[1px] border-mainAccent px-3 sm:px-6 lg:pt-7 xl:pt-12"
       >
         <h2
-          className={`w-full font-heading text-[14vw] font-bold uppercase tracking-tighter transition-none sm:translate-y-0 lg:text-[12vw] ${
+          className={`text-sectionText w-full font-heading font-bold uppercase tracking-tighter transition-none sm:translate-y-0 ${
             isExpanded ? "" : "overflow-hidden"
           }`}
         >
           {bannerTitle[0].id} {bannerTitle[0].title}
         </h2>
 
-        <button className="relative flex size-8 items-center justify-center rounded-full border border-black sm:size-10 xl:size-14">
+        <button
+          className="relative flex size-8 items-center justify-center rounded-full border border-black sm:size-10 xl:size-14"
+          aria-expanded={isExpanded}
+        >
           {/* Plus-minus icon */}
-          <span className="absolute block h-[1.3px] w-4 bg-black"></span>
+          <span className="absolute block h-[1.3px] w-3 bg-black sm:w-4"></span>
           <span
-            className={`absolute block h-[1px] w-4 bg-black transition-transform duration-300 ease-in-out ${
+            className={`absolute block h-[1px] w-3 bg-black transition-transform duration-300 ease-in-out sm:w-4 ${
               isExpanded ? "rotate-0" : "rotate-90"
             }`}
           ></span>
@@ -34,7 +37,7 @@ export default function SectionBanner({ bannerTitle, children }) {
 
       <div
         className={`banner-content transition-all duration-700 ease-in-out ${
-          isExpanded ? "max-h-[1000px]" : "max-h-0"
+          isExpanded ? "max-h-screen" : "max-h-0"
         } overflow-hidden`}
       >
         {children}
