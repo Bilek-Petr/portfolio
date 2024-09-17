3529bf33351e27cf9d00daea285368ce3ca605a1
Bilek-Petr <p.bilek29@gmail.com>
2024-09-17 11:06:05 +0200

    fixed responsivitiy

--- src/components/Sections/MyJourney.jsx
+++ src/components/Sections/MyJourney.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import SectionBanner from "../Common/SectionBanner";
+import Card from "../Common/Card";
 
 export default function MyJourney({ sectionMyJourneyData, sectionList }) {
   return (
     <section id="my-journey">
       <SectionBanner bannerTitle={sectionList}>
-        {sectionMyJourneyData.cards.map((card, index) => (
-          <div key={index}>
-            <p>{card.title}</p>
-            <p>{card.content}</p>
-          </div>
-        ))}
+        {/* Storyline, hidden on large, replaced by SVG */}
+        <span className="absolute left-1/2 top-1/2 z-0 h-[90%] w-[2px] -translate-x-1/2 -translate-y-1/2 transform bg-mainAccent lg:h-[15px] lg:w-[90%] lg:max-w-[1700px]"></span>
+
+        <div className="relative z-10 m-auto flex flex-col items-center gap-10 pb-10 lg:max-w-[1800px] lg:flex-row lg:gap-3 lg:px-3 lg:py-20">
+          {sectionMyJourneyData.cards.map((card) => (
+            <Card key={card.id} cardContent={card} />
+          ))}
+        </div>
       </SectionBanner>
     </section>
   );
