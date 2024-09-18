e7e135e3878ac63236f6f18208ede2ce9b699802
Bilek-Petr <p.bilek29@gmail.com>
2024-09-18 12:44:58 +0200

    simplified code into 2 children components

--- src/components/TechStack/Techstack.jsx
+++ src/components/TechStack/Techstack.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import IconWrapper from "./IconWrapper";
+import ToolName from "./ToolName";
+import { generateRandomGradient } from "../../utilities/getRandomGradient";
 import {
   FaSass,
   FaHtml5,
@@ -8,21 +11,35 @@ import {
   FaJs,
 } from "react-icons/fa6";
 import { SiPostman, SiTailwindcss, SiMongodb, SiFigma } from "react-icons/si";
-import { generateRandomGradient } from "../../utilities/getRandomGradient";
+
+const toolIcons = {
+  html: FaHtml5,
+  css: FaCss3Alt,
+  sass: FaSass,
+  tailwind: SiTailwindcss,
+  javascript: FaJs,
+  react: FaReact,
+  mongodb: SiMongodb,
+  git: FaGitAlt,
+  postman: SiPostman,
+  figma: SiFigma,
+};
 
 export default function Techstack({ techData }) {
-  const [hoveredGradient, setHoveredGradient] = useState({});
-  const toolIcons = {
-    html: FaHtml5,
-    css: FaCss3Alt,
-    sass: FaSass,
-    tailwind: SiTailwindcss,
-    javascript: FaJs,
-    react: FaReact,
-    mongodb: SiMongodb,
-    git: FaGitAlt,
-    postman: SiPostman,
-    figma: SiFigma,
+  const [hoveredGradients, setHoveredGradients] = useState({});
+
+  const handleMouseEnter = (index) => {
+    setHoveredGradients((prev) => ({
+      ...prev,
+      [index]: generateRandomGradient(),
+    }));
+  };
+
+  const handleMouseLeave = (index) => {
+    setHoveredGradients((prev) => ({
+      ...prev,
+      [index]: "",
+    }));
   };
 
   return (
@@ -31,24 +48,9 @@ export default function Techstack({ techData }) {
         Tech Stack
       </h2>
       <div className="m-auto mb-8 flex max-w-screen-2xl flex-col overflow-hidden">
-        {/* Container for Icons */}
         <div className="flex flex-wrap items-center justify-center gap-y-12 py-8 sm:gap-y-6">
           {techData.tools.map((tool, index) => {
             const IconComponent = toolIcons[tool];
-
-            const handleMouseEnter = (index) => {
-              setHoveredGradient((prev) => ({
-                ...prev,
-                [index]: generateRandomGradient(),
-              }));
-            };
-
-            const handleMouseLeave = (index) => {
-              setHoveredGradient((prev) => ({
-                ...prev,
-                [index]: "",
-              }));
-            };
 
             return (
               <div
@@ -57,20 +59,11 @@ export default function Techstack({ techData }) {
                 onMouseEnter={() => handleMouseEnter(index)}
                 onMouseLeave={() => handleMouseLeave(index)}
               >
-                {/* Icon */}
-                <div
-                  className="relative flex items-center justify-center overflow-hidden rounded-full border-[1px] border-mainAccent p-5 transition-transform duration-300 group-hover:scale-125"
-                  style={{ background: hoveredGradient[index] }}
-                >
-                  {IconComponent && (
-                    <IconComponent className="text-[3rem] transition-transform duration-300 sm:text-[4rem] lg:text-[6rem]" />
-                  )}
-                </div>
-
-                {/* Tool Name */}
-                <span className="absolute top-[-2.25rem] text-center font-heading text-sm font-bold uppercase opacity-0 transition-opacity duration-300 group-hover:opacity-100 sm:text-lg lg:text-xl">
-                  {tool}
-                </span>
+                <IconWrapper
+                  IconComponent={IconComponent}
+                  gradient={hoveredGradients[index]}
+                />
+                <ToolName name={tool} />
               </div>
             );
           })}
