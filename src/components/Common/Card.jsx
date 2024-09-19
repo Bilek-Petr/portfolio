14e37b9e05104dfa80aed5fe23eb6fca2a74dbda
Bilek-Petr <p.bilek29@gmail.com>
2024-09-19 13:52:36 +0200

    fixed card ford bigger screen sizes

--- src/components/Common/Card.jsx
+++ src/components/Common/Card.jsx
@@ -2,18 +2,28 @@ import React from "react";
 
 export default function Card({ cardContent: { title, field, content, year } }) {
   return (
-    <article className="container relative m-auto flex w-11/12 max-w-[700px] flex-col items-center justify-center rounded-sm border-[1px] border-mainAccent bg-white py-5 text-center shadow-custom-black duration-300 hover:bg-gray-100 hover:shadow-custom-black-hover lg:h-[25rem]">
+    <article className="container relative m-auto w-11/12 max-w-[1700px] items-center rounded-sm border-[1px] border-mainAccent bg-white py-5 text-center shadow-custom-black duration-300 hover:bg-gray-100 hover:shadow-custom-black-hover lg:grid lg:h-[20rem] lg:grid-rows-3">
       {year && (
         <span className="duration-400 absolute right-1 top-0 -translate-y-5 font-bold">
           {year}
         </span>
       )}
-
-      <header>
+      <header className="self-end">
         <p className="text-lg font-semibold">{title}</p>
-        {field && <p className="mt-2 font-bold text-mainAccent">{field}</p>}
+        {field && (
+          <p className="mt-2 text-lg font-bold text-mainAccent lg:mt-0">
+            {field}
+          </p>
+        )}
       </header>
-      {content && <section className="mt-2 opacity-80">{content}</section>}
+
+      <hr className="hidden h-[1px] w-full bg-gray-300 lg:block"></hr>
+
+      {content && (
+        <section className="mt-2 self-start opacity-80 lg:mt-0">
+          {content}
+        </section>
+      )}
     </article>
   );
 }
