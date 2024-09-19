a9b97d531e88e353300e2d15e15b8740cdb0bb24
Bilek-Petr <p.bilek29@gmail.com>
2024-09-19 13:53:31 +0200

    fixed component responsivity

--- src/components/Sections/MyJourney.jsx
+++ src/components/Sections/MyJourney.jsx
@@ -7,7 +7,7 @@ export default function MyJourney({ sectionMyJourneyData, sectionList }) {
     <section id="my-journey">
       <SectionBanner bannerTitle={sectionList}>
         {/* Storyline, hidden on large, replaced by SVG */}
-        <span className="absolute left-1/2 top-1/2 z-0 h-[90%] w-[2px] -translate-x-1/2 -translate-y-1/2 transform bg-mainAccent lg:h-[15px] lg:w-[90%] lg:max-w-[1700px]"></span>
+        <span className="absolute left-1/2 top-1/2 z-0 h-[85%] w-[2px] -translate-x-1/2 -translate-y-1/2 transform bg-mainAccent lg:h-[15px] lg:w-[90%] lg:max-w-[1700px]"></span>
 
         <div className="relative z-10 m-auto flex flex-col items-center gap-10 pb-10 lg:max-w-[1800px] lg:flex-row lg:gap-3 lg:px-3 lg:py-20">
           {sectionMyJourneyData.cards.map((card) => (
